5568590bc6d9d9f69b48fd99f67106d7cef61d58
Will-guann <willg0430@gmail.com>
2026-03-14 14:06:42 +1100

    suppresssing another hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn('font-body antialiased', fontInter.variable)}>
+      <body className={cn('font-body antialiased', fontInter.variable)} suppressHydrationWarning>
         <FirebaseClientProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
             {children}
